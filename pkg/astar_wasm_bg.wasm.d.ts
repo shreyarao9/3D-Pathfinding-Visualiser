dec7159d90bfed467908e924b422d5130202a8da
shreyarao9 <luthien2209@gmail.com>
2025-05-05 00:08:05 +0530

    add pkg for deployment

---
+++ pkg/astar_wasm_bg.wasm.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_pathfinding_free: (a: number, b: number) => void;
+export const pathfinding_new: (a: number, b: number) => number;
+export const pathfinding_set_obstacle: (a: number, b: number, c: number) => void;
+export const pathfinding_clear_obstacle: (a: number, b: number, c: number) => void;
+export const pathfinding_find_path: (a: number, b: number, c: number, d: number, e: number) => [number, number];
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __externref_drop_slice: (a: number, b: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
